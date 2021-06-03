70cd1135d8a74d14920bc8621d9b0cef2493c5bd
pratik2550 <pratikpatle250@gmail.com>
2021-06-04 01:25:07 +0530

    Tested API call

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.covidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.covidtracker.repository.SuperheroRepository
+import com.example.covidtracker.viewmodel.SuperHeroViewModel
+import com.example.covidtracker.viewmodel.SuperHeroViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SuperHeroViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("All superhero", "Pratik")
+        val repository = SuperheroRepository()
+        val viewModelFactory = SuperHeroViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(SuperHeroViewModel::class.java)
+        viewModel.getAllSuperhero()
+        viewModel.myResponse.observe(this, Observer { response->
+            Log.i("All superhero", response.toString())
+        })
     }
 }
