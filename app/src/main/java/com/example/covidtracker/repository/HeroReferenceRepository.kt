dcd74f23e31d924a3140305c0d9afb199411d8fb
pratik2550 <pratikpatle250@gmail.com>
2021-06-06 00:28:52 +0530

    Added search adapter and design search view.

--- app/src/main/java/com/example/covidtracker/repository/HeroReferenceRepository.kt
+++ app/src/main/java/com/example/covidtracker/repository/HeroReferenceRepository.kt
@@ -6,11 +6,9 @@ import com.example.covidtracker.model.superheroSearch
 import retrofit2.Response
 
 class HeroReferenceRepository {
-    suspend fun searchSuperhero(id: String): Response<superheroSearch> {
-        return ReferenceSuperhero.api.searchSuperhero(id)
-    }
+    suspend fun searchSuperhero(id: String) = ReferenceSuperhero.api.searchSuperhero(id)
 
-    suspend fun getById(hero: String): Response<Superhero> {
-        return ReferenceSuperhero.api.getById(hero)
-    }
+
+    suspend fun getById(hero: String) = ReferenceSuperhero.api.getById(hero)
+
 }
---
+++ app/src/main/java/com/example/covidtracker/ui/adapter/SearchAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.covidtracker.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.covidtracker.R
+import com.example.covidtracker.model.Superhero
+import kotlinx.android.synthetic.main.raw_search_heroes.view.*
+
+class SearchAdapter(private val heroList: List<Superhero>, private val context: Context) : RecyclerView.Adapter<SearchAdapter.Holder>() {
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val viewHolder =
+            Holder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.raw_search_heroes, parent, false)
+            )
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val currItem = heroList[position]
+        holder.itemView.tvName.text = currItem.name
+        Glide.with(context).load(currItem.image?.url).circleCrop().diskCacheStrategy(
+            DiskCacheStrategy.ALL).into(holder.itemView.ivSearchSuperhero)
+    }
+
+    override fun getItemCount(): Int {
+        return heroList.size
+    }
+}
+interface OnSearchedItemClickListener{
+    fun onItemClick(position: Int)
+}
